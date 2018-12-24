c9e4eda6d799af5902751d57a0a654d6d707bb53
cwinters8 <clwinters8@gmail.com>
2018-12-24 14:15:31 -0600

    add Other job role input and event handler

--- js/app.js
+++ js/app.js
@@ -2,3 +2,16 @@
 $(document).ready(() => {
     $('#name').focus();
 })
+
+// only display other-title input if 'Other' job role is selected
+const titles = $('#title');
+const otherTitle = $('#other-title');
+otherTitle.hide();
+titles.change((e) => {
+    if (e.target.value === 'other') {
+        otherTitle.show();
+        otherTitle.focus();
+    } else {
+        otherTitle.hide();
+    }
+})
